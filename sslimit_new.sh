dff5b5625aa843b01c49c751521a974b0cca324b
KyuAsahi <49791516+KyuAsahi@users.noreply.github.com>
2021-08-31 17:44:48 +0800

    Update sslimit_new.sh

--- sslimit_new.sh
+++ sslimit_new.sh
@@ -7,28 +7,28 @@ max_link=5
 
 while [ ! ]
 do
-    for container in $container_list
-    do
-        PID=`docker inspect -f '{{.State.Pid}}' $container`
-        Container_IP=`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $container`
-        link_num=`nsenter -t $PID -n netstat -anp|grep ESTABLISHED |grep 8388 |awk '{print $5}'|awk -F ':' '{print $1}'|sort|uniq|wc -l`
-        iptables -C DOCKER-USER -p tcp -d $Container_IP -j DROP
-        iptables_flag=`echo $?`
+	for container in $container_list
+	do
+		PID=`docker inspect -f '{{.State.Pid}}' $container`
+		Container_IP=`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $container`
+		link_num=`nsenter -t $PID -n netstat -anp|grep ESTABLISHED |grep 8388 |awk '{print $5}'|awk -F ':' '{print $1}'|sort|uniq|wc -l`
+		iptables -C DOCKER-USER -p tcp -d $Container_IP -j DROP
+		iptables_flag=`echo $?`
 		echo "--------------------------------"
 		date
-        echo "$container link num is $link_num"
-        if [ $link_num -gt $max_link ]
-        then
-            if [ $iptables_flag -eq 1 ]
-            then
-            iptables -I DOCKER-USER -p tcp -d $Container_IP -j DROP
-            fi
-        else
-            if [ $iptables_flag -eq 0 ]
-            then
-            iptables -D DOCKER-USER -p tcp -d $Container_IP -j DROP
-            fi
-        fi
-    done
-    sleep 600
+		echo "$container link num is $link_num"
+		if [ $link_num -gt $max_link ]
+		then
+			if [ $iptables_flag -eq 1 ]
+			then
+			iptables -I DOCKER-USER -p tcp -d $Container_IP -j DROP
+			fi
+		else
+			if [ $iptables_flag -eq 0 ]
+			then
+			iptables -D DOCKER-USER -p tcp -d $Container_IP -j DROP
+			fi
+		fi
+	done
+	sleep 600
 done

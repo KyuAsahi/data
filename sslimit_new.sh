bf91d122cc7a565014c06d85a97cae1d795cbdfe
KyuAsahi <49791516+KyuAsahi@users.noreply.github.com>
2021-09-02 17:11:49 +0800

    Update sslimit_new.sh

--- sslimit_new.sh
+++ sslimit_new.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
 
-local_path=`pwd`
-ss_yml_path="$local_path/docker-compose.yml"
-container_list=`cat $ss_yml_path |grep container_name |grep -oP '(?<=:\s)ss_\w*'`
 max_link=5
 
-while [ ! ]
+script_path=`realpath $0`
+script_dir=`dirname $script_path`
+ss_yml_path="$script_dir/docker-compose.yml"
+
+container_list=`cat $ss_yml_path |grep container_name |grep -oP '(?<=:\s)ss_\w*'`
+
+echo "--------------------------------"
+TZ="Asia/Shanghai" date
+for container in $container_list
 do
-	echo "--------------------------------"
-	date
-	for container in $container_list
-	do
-		PID=`docker inspect -f '{{.State.Pid}}' $container`
-		Container_IP=`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $container`
-		link_num=`nsenter -t $PID -n netstat -anp|grep ESTABLISHED |grep 8388 |awk '{print $5}'|awk -F ':' '{print $1}'|sort|uniq|wc -l`
-		iptables -C DOCKER-USER -p tcp -d $Container_IP -j DROP
-		iptables_flag=`echo $?`
-		echo "$container link num is $link_num"
-		if [ $link_num -gt $max_link ]
+	PID=`docker inspect -f '{{.State.Pid}}' $container`
+	Container_IP=`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $container`
+	link_num=`nsenter -t $PID -n netstat -anp|grep ESTABLISHED |grep 8388 |awk '{print $5}'|awk -F ':' '{print $1}'|sort|uniq|wc -l`
+	iptables -C DOCKER-USER -p tcp -d $Container_IP -j DROP > /dev/null
+	iptables_flag=`echo $?`
+	echo "$container link num is $link_num"
+	if [ $link_num -gt $max_link ]
+	then
+		if [ $iptables_flag -eq 1 ]
 		then
-			if [ $iptables_flag -eq 1 ]
-			then
-			iptables -I DOCKER-USER -p tcp -d $Container_IP -j DROP
-			fi
-		else
-			if [ $iptables_flag -eq 0 ]
-			then
-			iptables -D DOCKER-USER -p tcp -d $Container_IP -j DROP
-			fi
+		iptables -I DOCKER-USER -p tcp -d $Container_IP -j DROP
 		fi
-	done
-	sleep 600
+	else
+		if [ $iptables_flag -eq 0 ]
+		then
+		iptables -D DOCKER-USER -p tcp -d $Container_IP -j DROP
+		fi
+	fi
 done
+iptables -L DOCKER-USER

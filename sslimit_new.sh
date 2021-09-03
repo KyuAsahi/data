8fc94cd2ba72b4204378b22e90ad11951975e72e
KyuAsahi <49791516+KyuAsahi@users.noreply.github.com>
2021-09-03 17:44:37 +0800

    Update sslimit_new.sh

--- sslimit_new.sh
+++ sslimit_new.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 max_link=5
 
 script_path=`realpath $0`
@@ -19,7 +17,7 @@ do
 	link_num=`nsenter -t $PID -n netstat -anp|grep ESTABLISHED |grep 8388 |awk '{print $5}'|awk -F ':' '{print $1}'|sort|uniq|wc -l`
 	iptables -C DOCKER-USER -p tcp -d $Container_IP -j DROP > /dev/null 2>&1
 	iptables_flag=`echo $?`
-	echo "$container link num is $link_num"
+	#echo "$container link num is $link_num"
 	if [ $link_num -gt $max_link ]
 	then
 		if [ $iptables_flag -eq 1 ]
@@ -45,12 +43,16 @@ do
 	this_sum=`cat /proc/$PID/net/dev |grep eth0| awk '{print $10}'`
 	if [ $this_sum -ge $last_sum ]
 	then
-		dif=expr $this_sum - $last_sum
+		dif=`expr $this_sum - $last_sum`
 	else
-		dif=expr $this_sum - 0
+		dif=`expr $this_sum - 0`
 	fi
-	total_sum =expr $total_sum + $dif
+	total_sum=`expr $total_sum + $dif`
+	total_sum_kb=`expr $total_sum / 1024`
+	total_sum_mb=`expr $total_sum_kb / 1024`
+	total_sum_gb=`expr $total_sum_mb / 1024`
 	echo $this_sum > $traffic_dir/${container}_last
 	echo $total_sum > $traffic_dir/${container}_total
+	echo "$container link num is $link_num,total $total_sum_gb GB,$total_sum_mb MB,$total_sum_kb KB"
 done
 iptables -L DOCKER-USER
